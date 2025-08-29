a83808fe13007ba18ea5e27dbb61db8c65b7a811
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-08-29 16:29:55 -0700

    Init draft

---
+++ src/services/WinGetStudio.Services.Telemetry/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.DependencyInjection;
+using WinGetStudio.Services.Core.Extensions;
+using WinGetStudio.Services.Telemetry.Contracts;
+
+namespace WinGetStudio.Services.Telemetry.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddDSC(this IServiceCollection services)
+    {
+        services.AddCore();
+        services.AddSingleton<ITelemetry, Services.Telemetry>();
+        return services;
+    }
+}
---
+++ src/services/WinGetStudio.Services.Telemetry/Services/Telemetry.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Telemetry;
+using WinGetStudio.Services.Telemetry.Contracts;
+using WinGetStudio.Services.Telemetry.Models;
+
+namespace WinGetStudio.Services.Telemetry.Services;
+
+/// <summary>
+/// Telemetry service implementation.
+/// </summary>
+internal sealed partial class Telemetry : TelemetryEventSource, ITelemetry
+{
+    private readonly TelemetryEventListener _telemetryEventListener;
+    private const string EventSourceName = "Microsoft.WinGetStudio";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Telemetry"/> class.
+    /// </summary>
+    public Telemetry()
+        : base(EventSourceName, TelemetryGroup.MicrosoftTelemetry)
+    {
+        _telemetryEventListener = new(this);
+    }
+
+    /// <inheritdoc/>
+    public void WriteEvent<T>(T telemetryEvent)
+        where T : EventBase
+    {
+        Write<T>(
+            null,
+            new EventSourceOptions()
+            {
+                Keywords = CriticalDataKeyword,
+            },
+            telemetryEvent);
+
+        EventBase.IncrementCorrelationVector();
+    }
+
+    /// <inheritdoc/>
+    public void DisableEvents()
+    {
+        _telemetryEventListener.DisableEvents();
+    }
+}
