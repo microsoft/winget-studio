4ba2107e031dc373f85321d606d4c1aaafe5a2b1
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-09-08 13:06:15 -0700

    WIP

---
+++ src/WinGetStudio/Services/UIFeedbackService.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using WinGetStudio.Contracts.Services;
+
+namespace WinGetStudio.Services;
+
+internal sealed class UIFeedbackService : IUIFeedbackService
+{
+}
