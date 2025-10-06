b54ceedd09a0971d9514086b17098afa489e45b0
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-10-06 16:06:48 -0700

    Added comments

--- src/WinGetStudio/Models/Theme.cs
+++ src/WinGetStudio/Models/Theme.cs
@@ -5,10 +5,19 @@ using Microsoft.UI.Xaml;
 
 namespace WinGetStudio.Models;
 
+/// <summary>
+/// Represents a theme option for the application.
+/// </summary>
 public sealed class Theme
 {
+    /// <summary>
+    /// Gets the name of the theme.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// Gets the corresponding ElementTheme value.
+    /// </summary>
     public ElementTheme ElementTheme { get; }
 
     public Theme(string name, ElementTheme elementTheme)
