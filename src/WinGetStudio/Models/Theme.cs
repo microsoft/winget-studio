952d08160ac3ffc60a3d7fac3076d11207a4cc1a
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-10-06 15:45:01 -0700

    WIP

---
+++ src/WinGetStudio/Models/Theme.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+
+namespace WinGetStudio.Models;
+
+public sealed class Theme
+{
+    public string Name { get; }
+
+    public ElementTheme ElementTheme { get; }
+
+    public Theme(string name, ElementTheme elementTheme)
+    {
+        Name = name;
+        ElementTheme = elementTheme;
+    }
+}
