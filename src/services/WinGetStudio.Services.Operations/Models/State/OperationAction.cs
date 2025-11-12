87d7a3e784dfd40aacb445ef1d01de0ea8c38309
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-11-11 16:46:00 -0800

    Added action id

--- src/services/WinGetStudio.Services.Operations/Models/State/OperationAction.cs
+++ src/services/WinGetStudio.Services.Operations/Models/State/OperationAction.cs
@@ -9,7 +9,11 @@ namespace WinGetStudio.Services.Operations.Models.State;
 /// <summary>
 /// Represents an action that can be taken for an operation.
 /// </summary>
+/// <param name="Id">The unique identifier for the action.</param>
 /// <param name="Text">The text to display for the action.</param>
 /// <param name="IsPrimary">Whether the action is a primary action.</param>
 /// <param name="Action">The action to perform.</param>
-public sealed record class OperationAction(string Text, bool IsPrimary, Func<Task> Action);
+public sealed record class OperationAction(string Text, bool IsPrimary, Func<Task> Action)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
